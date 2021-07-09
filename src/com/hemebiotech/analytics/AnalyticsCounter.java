51cfc3a05152ec20cafa500ce4a85298e538d6ad
Yazid IGGUI <Vous@exemple.comDizayb1@hotmail.comDizayb1@hotmail.com>
2021-07-09 23:30:30 +0200

    Creation of the new branch

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,58 +1,15 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class AnalyticsCounter {
 
-	
-		private static int headacheCount = 0;	
-		private static int rashCount = 0;		
-		private static int pupilCount = 0;		
+		
 		
 		public static void main(String args[]){
-			try {
-			BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-			String line = reader.readLine();
-
-			int i = 0;	
 			
-			while (line != null) {
-				i++;	
-				System.out.println("symptom from file: " + line);
-				if (line.equals("headache")) {
-					headacheCount++;
-					System.out.println("number of headaches: " + headacheCount);
-				}
-				else if (line.equals("rash")) {
-					rashCount++;
-				}
-				else if (line.contains("pupils")) {
-					pupilCount++;
-				}
-
-				line = reader.readLine();
-				
-				
-			}reader.close();
-			}catch (FileNotFoundException e) {
-				
-				System.err.printf(" file not found !");
-			} catch(IOException e) {
-				System.out.println("a problem occurred when the tomporary drive was closed");
-			}
-			try {
-			FileWriter writer = new FileWriter ("result.out");
-			writer.write("headache: " + headacheCount + "\n");
-			writer.write("rash: " + rashCount + "\n");
-			writer.write("dialated pupils: " + pupilCount + "\n");
-			writer.close();
-		}catch(IOException e) {
-			System.out.println("a problem occurred when closing PrintWriter");
+			ReadSymptomDataFromFile rsdff =new ReadSymptomDataFromFile("symptoms.txt");
+			rsdff.GetSymptoms0();
+			rsdff.valueOfSymptoms();
+			rsdff.fileResultOut();
 		}
-		}
-
 }
---
+++ src/com/hemebiotech/analytics/ISymptomReader.java
@@ -0,0 +1,11 @@
+package com.hemebiotech.analytics;
+
+import java.util.Map;
+
+public interface ISymptomReader {
+
+	
+	public Map<Integer, String> GetSymptoms0() ;
+	public void valueOfSymptoms() ;
+	public void fileResultOut() ;
+}
