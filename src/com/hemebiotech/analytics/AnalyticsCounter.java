3dbd4373e7c16473c0387ba9cf360d108546d688
Yazid IGGUI <Vous@exemple.comDizayb1@hotmail.comDizayb1@hotmail.com>
2021-07-09 13:22:32 +0200

    code cleaning and variable and condition correction

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,46 +1,58 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AnalyticsCounter {
 
 	
-		private static int headacheCount = 0;	// initialize to 0
-		private static int rashCount = 0;		// initialize to 0
-		private static int pupilCount = 0;		// initialize to 0
+		private static int headacheCount = 0;	
+		private static int rashCount = 0;		
+		private static int pupilCount = 0;		
 		
-		public static void main(String args[]) throws Exception {
-			// first get input
+		public static void main(String args[]){
+			try {
 			BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 			String line = reader.readLine();
 
-			int i = 0;	// set i to 0
-			int headCount = 0;	// counts headaches
+			int i = 0;	
+			
 			while (line != null) {
-				i++;	// increment i
+				i++;	
 				System.out.println("symptom from file: " + line);
 				if (line.equals("headache")) {
-					headCount++;
-					System.out.println("number of headaches: " + headCount);
+					headacheCount++;
+					System.out.println("number of headaches: " + headacheCount);
 				}
-				else if (line.equals("rush")) {
+				else if (line.equals("rash")) {
 					rashCount++;
 				}
 				else if (line.contains("pupils")) {
 					pupilCount++;
 				}
 
-				line = reader.readLine();	// get another symptom
+				line = reader.readLine();
+				
+				
+			}reader.close();
+			}catch (FileNotFoundException e) {
+				
+				System.err.printf(" file not found !");
+			} catch(IOException e) {
+				System.out.println("a problem occurred when the tomporary drive was closed");
 			}
-			
-			// next generate output
+			try {
 			FileWriter writer = new FileWriter ("result.out");
 			writer.write("headache: " + headacheCount + "\n");
 			writer.write("rash: " + rashCount + "\n");
 			writer.write("dialated pupils: " + pupilCount + "\n");
 			writer.close();
+		}catch(IOException e) {
+			System.out.println("a problem occurred when closing PrintWriter");
+		}
 		}
 
 }
